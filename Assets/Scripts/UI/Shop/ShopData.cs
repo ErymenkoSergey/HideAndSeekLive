e7ef5d26dd39f84c9b9f0d542c2b089d9f27d36d
ErymenkoSergey <eryomenkosergey10@gmail.com>
2023-03-05 13:28:18 +0700

    Added new Map.

--- Assets/Scripts/UI/Shop/ShopData.cs
+++ Assets/Scripts/UI/Shop/ShopData.cs
@@ -27,11 +27,11 @@ public class ShopData : ScriptableObject
     {
         var skinHiding = SkinShopItems[index].Prefab.GetComponent<bl_PlayerNetwork>();
         skinHiding.GetComponent<bl_PlayerSettings>().PlayerTeam = Team.Hiding;
-        bl_GameData.Instance.Player1 = skinHiding;// SkinShopItems[index].Prefab.GetComponent<bl_PlayerNetwork>();
+        bl_GameData.Instance.Player1 = skinHiding;
 
         var skinManiac = SkinShopItems[index].Prefab.GetComponent<bl_PlayerNetwork>();
         skinManiac.GetComponent<bl_PlayerSettings>().PlayerTeam = Team.Maniac;
-        bl_GameData.Instance.Player2 = skinManiac;// SkinShopItems[index].Prefab.GetComponent<bl_PlayerNetwork>();
+        bl_GameData.Instance.Player2 = skinManiac;
     }
 
     [Serializable]
