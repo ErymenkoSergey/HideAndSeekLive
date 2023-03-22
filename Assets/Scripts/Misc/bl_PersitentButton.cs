21875f9b52b18d7ce732779f5348c3d46a6099c7
ErymenkoSergey <eryomenkosergey10@gmail.com>
2023-03-22 10:13:28 +0700

    Added optimization.

--- Assets/Scripts/Misc/bl_PersitentButton.cs
+++ Assets/Scripts/Misc/bl_PersitentButton.cs
@@ -46,6 +46,11 @@ namespace Lovatto.MobileInput
 #if !UNITY_EDITOR
           //  StartCoroutine(OnUpdate());
 #endif
+
+            if (lastId == -2)
+            {
+                lastId = eventData.pointerId;
+            }
         }
 
         /// <summary>
@@ -59,9 +64,15 @@ namespace Lovatto.MobileInput
            // StopAllCoroutines();
 #endif
             m_Transform.anchoredPosition = defaultPosition;
-            SetMouse(0f, 0f);
-        }
 
+            if (eventData.pointerId == lastId)
+            {
+                lastId = -2;
+                SetMouse(0f, 0f);
+                
+            } 
+        }
+        private int lastId = -2;
         /// <summary>
         /// 
         /// </summary>
@@ -70,25 +81,25 @@ namespace Lovatto.MobileInput
         {
             if (!init) return;
 
-            
+
             //SetMouse(eventD);
-
-            Vector2 pos;
-            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(m_Transform, eventData.position, null, out pos))
+            if (eventData.pointerId == lastId)
             {
-                pos.x = (pos.x / m_Transform.sizeDelta.x);
-                pos.y = (pos.y / m_Transform.sizeDelta.y);
+                Vector2 pos;
 
-                //pos.x = (m_Transform.sizeDelta.x);
-                //pos.y = (m_Transform.sizeDelta.y);
-
-                inputVector = new Vector3(pos.x, 0, pos.y);
-                inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized * 10f : inputVector;
-
-                //stickPosition.x = inputVector.x * (m_Transform.sizeDelta.x * ClampArea);
-                //stickPosition.y = inputVector.z * (m_Transform.sizeDelta.y * ClampArea);
-                SetMouse(inputVector.x, inputVector.z);
-                //StickRect.anchoredPosition = stickPosition;
+                if (RectTransformUtility.ScreenPointToLocalPointInRectangle(m_Transform, eventData.position, null, out pos))
+                {
+                    //pos.x = (pos.x / m_Transform.sizeDelta.x);
+                    //pos.y = (pos.y / m_Transform.sizeDelta.y);
+                   
+                    inputVector = new Vector3(pos.x, 0, pos.y);
+                    inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector.normalized;
+                    Debug.Log($"OnDrag {pos.x} / {pos.y}, inputVector {inputVector}");
+                    //stickPosition.x = inputVector.x * (m_Transform.sizeDelta.x * ClampArea);
+                    //stickPosition.y = inputVector.z * (m_Transform.sizeDelta.y * ClampArea);
+                    SetMouse(inputVector.x, inputVector.z);
+                    //m_Transform.anchoredPosition = stickPosition;
+                }
             }
 
             if (Clamped)
@@ -101,7 +112,9 @@ namespace Lovatto.MobileInput
 
             m_Transform.position = eventData.position;
         }
+
         //private Vector2 stickPosition = Vector2.zero;
+        private Vector2 stickPosition = Vector2.zero;
         public Vector3 inputVector { get; set; }
         //private void ConvertVector()
         //{
